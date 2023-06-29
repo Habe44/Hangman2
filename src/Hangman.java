ea0e532007e30c73f963b1a37bf0904e689cab19
Habe44 <121323716+Habe44@users.noreply.github.com>
2023-06-29 19:46:13 +0200

    :)

--- src/Hangman.java
+++ src/Hangman.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Locale;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -9,13 +10,29 @@ public class Hangman {
     String randomWord;
     String playerLetter;
     String x = "_";
+    int win = 0;
+    int lose = 0;
 
 
    void Draw() {
         Random r = new Random();
         int randomItem = r.nextInt(Hangman.words.size());
         randomWord = Hangman.words.get(randomItem);
-
+        if(randomWord.equals("Puszka")){
+            System.out.println("Podpowiedz: blaszane opakowanie np. napoju.");
+        }
+        if(randomWord.equals("Pies")){
+           System.out.println("Podpowiedz: mowia, ze jest to najlepszy przyjaciel czlowieka.");
+       }
+        if(randomWord.equals("Obraz")){
+           System.out.println("Podpowiedz: podziwia sie go na wystawie.");
+       }
+       if(randomWord.equals("Drewno")){
+           System.out.println("Podpowiedz: surowiec dla ciesli.");
+       }
+       if(randomWord.equals("Butelka")){
+           System.out.println("Podpowiedz: szklane opakowanie");
+       }
     }
 
     void encrypt() {
@@ -34,16 +51,23 @@ public class Hangman {
                 System.out.println("Wykorzystana litera");
                 continue;
             }
-            if(randomWord.contains(playerLetter)){
+            if(randomWord.toLowerCase().contains(playerLetter)){
                 System.out.println("Zgadza sie !");
                 correctLetters.add(playerLetter);
             }
             if(correctLetters.size() == randomWord.length()){
+                System.out.println("");
                 System.out.println("Odgadles wszystkie litery");
+                win++;
                 System.out.println("Wylosowane haslo to: " + randomWord);
+                System.out.println("");
+                correctLetters.clear();
+                System.out.println("Wygrane: " + win);
+                System.out.println("Przegrane: " + lose);
+                System.out.println("");
                 break;
             }
-            if(!randomWord.contains(playerLetter)){
+            if(!randomWord.toLowerCase().contains(playerLetter)){
                 incorrectLetters.add(playerLetter);
                 if(incorrectLetters.size() == 1){
                     System.out.println("^^^^^^^");
@@ -119,20 +143,55 @@ public class Hangman {
                     System.out.println("|");
                     System.out.println("^^^^^^^");
                     System.out.println("Przegrales !");
+                    System.out.println("Wylosowane haslo to: " + randomWord);
+                    System.out.println("");
+                    incorrectLetters.clear();
+                    lose++;
+                    System.out.println("Wygrane: " + win);
+                    System.out.println("Przegrane: " + lose);
+                    System.out.println("");
                     break;
+                    }
                 }
             }
         }
-    }
-    public static void main(String[] args) {
-        words.add("");
-        words.add("");
-        words.add("");
-        Hangman h = new Hangman();
 
-        h.Draw();
-        h.encrypt();
-        h.decrypt();
+    public static void main(String[] args) {
+        words.add("Puszka");
+        words.add("Pies");
+        words.add("Obraz");
+        words.add("Drewno");
+        words.add("Butelka");
+        Scanner scn = new Scanner(System.in);
+        Hangman h = new Hangman();
+        boolean game = true;
+
+        do {
+            h.Draw();
+            h.encrypt();
+            h.decrypt();
+            if(h.win + h.lose == 5){
+                if(h.win > h.lose){
+                    System.out.println("Wygrales gre! Wynik:");
+                    System.out.println("Wygrane: " + h.win + "   " + "Przegrane: " + h.lose);
+                }
+                if(h.win < h.lose){
+                    System.out.println("Przegrales gre!");
+                    System.out.println("-----------------------");
+                    System.out.println("|                     |");
+                    System.out.println("|                     0");
+                    System.out.println("|                    >|<");
+                    System.out.println("|                     |");
+                    System.out.println("|                    ] [");
+                    System.out.println("|");
+                    System.out.println("|");
+                    System.out.println("^^^^^^^");
+                    System.out.println("Wynik:");
+                    System.out.println("Wygrane: " + h.win + "   " + "Przegrane: " + h.lose);
+                }
+            }
+        }
+        while(game);
 
     }
 }
